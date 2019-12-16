e0c9d89977d47b027ca69b965b3a81dd6885dbcd
unknown <52844667+a4444304@users.noreply.github.com>
2019-12-16 11:12:06 -0600

    updated config-routes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,15 @@
+// Server routes 
+// -------------
+
+// this will bring in the scrape 
+// function from our scripts directory
+var Scrape = require("../scripts/scrape");
+
+// this will bring headlines
+// and notes from the controller
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
 module.exports = function(router) {
     // this route renders the homepage
 router.get("/", function(req, res) {
@@ -6,5 +18,22 @@ router.get("/", function(req, res) {
     // this route renders the saved handlebars page
 router.get("/saved", function(req, res) {
     res.render("saved");
+});
+
+// we are creating an API route
+// to fetch all of the articles
+router.get("/api/fetch", function(req, res) {
+    headlinesController.fetch(function(err, docs) {
+        if (!docs || docs.insertedCount === 0) {
+            res.json({
+                message: "No new articles today. Check back tomorrow."
+            });
+        }
+        else {
+            res.json({
+                message: "Added" + docs.insertedCount + "new articles"
+            });
+        }
+    });
 });
 }
